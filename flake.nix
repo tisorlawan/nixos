d35f0b47632a46f150d752dfeaf27e8a8332092d
Agung Baptiso Sorlawan <tisorlawan@gmail.com>
2025-02-26 07:59:27 +0700

    update

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/73cf49b8";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/6313551c";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
 
     # iwmenu.url = "github:e-tho/iwmenu";
